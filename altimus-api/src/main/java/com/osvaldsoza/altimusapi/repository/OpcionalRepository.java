e4139e908352068a98d79ec6b64e221cc6acaa60
osvaldsoza <osvald.soza@gmail.com>
2019-11-22 01:11:24 -0300

    atualizacao

--- altimus-api/src/main/java/com/osvaldsoza/altimusapi/repository/OpcionalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.osvaldsoza.altimusapi.repository;
-
-import com.osvaldsoza.altimusapi.models.Opcional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OpcionalRepository  extends JpaRepository<Opcional,Long> {
-}
--- altimus-api/src/main/java/com/osvaldsoza/altimusapi/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.osvaldsoza.altimusapi.repository;
-
-import com.osvaldsoza.altimusapi.models.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
-
-     Usuario findByLogin(String login);
-}
